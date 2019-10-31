f7716943b54d8ab18e046386660961cb59606a64
Raman Nasovich <raman.nasovich@thomsonreuters.com>
2019-10-31 15:48:20 +0300

    moving routing component to separate file

--- App.tsx
+++ App.tsx
@@ -1,88 +1,11 @@
 import 'reflect-metadata';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createDrawerNavigator } from 'react-navigation-drawer';
-import { createStackNavigator } from 'react-navigation-stack';
-import AuthScreen from './src/screens/auth';
-import DashboardScreen from './src/screens/dashboard';
-import InitialScreen from './src/screens/initial';
-import CreateAccountScreen from './src/screens/create-account';
-import ForgotPasswordScreen from './src/screens/forgot-password';
-import AddSetScreen from './src/screens/add-set';
-import AddSuperSetScreen from './src/screens/add-superset';
-import DummyScreen from './src/screens/dummy';
 import { Ionicons } from '@expo/vector-icons';
 import * as React from 'react';
 import * as Font from 'expo-font';
-import { ContainerConfigurator } from './src/ioc/container-configurator';
-import { ConfigurationProvider } from './src/data-access/configuration-provider';
-import { container } from './src/ioc/container';
-import EditSetScreen from './src/screens/edit-set';
-import { Text, View, ActivityIndicator, StatusBar } from 'react-native';
-
-let configurationProvider = new ConfigurationProvider();
-new ContainerConfigurator(configurationProvider).configure(container);
-
-const dashboardStack = createStackNavigator({
-    Dashboard: { screen: DashboardScreen },
-    EditSet: { screen: EditSetScreen }
-}, {
-    headerMode: 'none',
-    navigationOptions: {
-        headerVisible: false
-    }
-});
-
-let drawerNavigatorRouteConfig = {
-    Dashboard: {
-        screen: dashboardStack,
-        navigationOptions: {
-            drawerLabel: 'Dashboard',
-            drawerIcon: () => <Ionicons name='md-home' size={30} />,
-            headerRight: <Text>Boom</Text>
-        }
-    },
-    AddSet: {
-        screen: AddSetScreen,
-        navigationOptions: {
-            drawerLabel: 'Add set',
-            drawerIcon: () => <Ionicons name='md-add' size={30} />
-        }
-    },
-    AddSuperSet: {
-        screen: AddSuperSetScreen,
-        navigationOptions: {
-            drawerLabel: 'Add superset',
-            drawerIcon: () => <Ionicons name='md-add' size={30} />
-        }
-    }
-};
-
-if (!configurationProvider.isInLocalMode()) {
-    drawerNavigatorRouteConfig['SignOut'] = {
-        // dirty hack to navigate to parent navigator's route: simply navigating 
-        // to 'Login' route instead of redirecting to relevant component
-        screen: DummyScreen,
-        navigationOptions: {
-            drawerLabel: 'Log out',
-            tabBarIcon: () => <Ionicons name='md-log-out' size={30} />
-        }
-    }
-}
-
-const mainAppNavigator = createDrawerNavigator(drawerNavigatorRouteConfig);
-
-const switchNavigator = createSwitchNavigator({
-    Initial: InitialScreen,
-    AuthorizedApp: mainAppNavigator as any,
-    Auth: AuthScreen,
-    CreateAccount: CreateAccountScreen,
-    ForgotPassword: ForgotPasswordScreen
-});
-
-const AppContainer = createAppContainer(switchNavigator);
+import { View, ActivityIndicator, StatusBar } from 'react-native';
+import { AppContainer } from './app-container';
 
 class App extends React.Component<any, any> {
-
     constructor(props) {
         super(props);
         this.state = {
@@ -91,11 +14,17 @@ class App extends React.Component<any, any> {
     }
     render() {
         if (this.state.isReady) {
-            return <View style={{ flex: 1, marginTop: StatusBar.currentHeight }}><AppContainer></AppContainer></View>;
+            return (
+                <View style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+                    <AppContainer></AppContainer>
+                </View>
+            );
         } else {
-            return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ActivityIndicator size='large' />
-            </View>
+            return (
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <ActivityIndicator size="large" />
+                </View>
+            );
         }
     }
 
---
+++ app-container.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { ConfigurationProvider } from './src/data-access/configuration-provider';
+import { ContainerConfigurator } from './src/ioc/container-configurator';
+import { container } from './src/ioc/container';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Ionicons } from '@expo/vector-icons';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import AuthScreen from './src/screens/auth';
+import DashboardScreen from './src/screens/dashboard';
+import InitialScreen from './src/screens/initial';
+import CreateAccountScreen from './src/screens/create-account';
+import ForgotPasswordScreen from './src/screens/forgot-password';
+import AddSetScreen from './src/screens/add-set';
+import AddSuperSetScreen from './src/screens/add-superset';
+import EditSetScreen from './src/screens/edit-set';
+import DummyScreen from './src/screens/dummy';
+import { Text } from 'react-native';
+
+let configurationProvider = new ConfigurationProvider();
+new ContainerConfigurator(configurationProvider).configure(container);
+
+const dashboardStack = createStackNavigator(
+    {
+        Dashboard: { screen: DashboardScreen },
+        EditSet: { screen: EditSetScreen }
+    },
+    {
+        headerMode: 'none',
+        navigationOptions: {
+            headerVisible: false
+        }
+    }
+);
+
+let drawerNavigatorRouteConfig = {
+    Dashboard: {
+        screen: dashboardStack,
+        navigationOptions: {
+            drawerLabel: 'Dashboard',
+            drawerIcon: () => <Ionicons name="md-home" size={30} />,
+            headerRight: <Text>Boom</Text>
+        }
+    },
+    AddSet: {
+        screen: AddSetScreen,
+        navigationOptions: {
+            drawerLabel: 'Add set',
+            drawerIcon: () => <Ionicons name="md-add" size={30} />
+        }
+    },
+    AddSuperSet: {
+        screen: AddSuperSetScreen,
+        navigationOptions: {
+            drawerLabel: 'Add superset',
+            drawerIcon: () => <Ionicons name="md-add" size={30} />
+        }
+    }
+};
+
+if (!configurationProvider.isInLocalMode()) {
+    drawerNavigatorRouteConfig['SignOut'] = {
+        // dirty hack to navigate to parent navigator's route: simply navigating
+        // to 'Login' route instead of redirecting to relevant component
+        screen: DummyScreen,
+        navigationOptions: {
+            drawerLabel: 'Log out',
+            tabBarIcon: () => <Ionicons name="md-log-out" size={30} />
+        }
+    };
+}
+
+const mainAppNavigator = createDrawerNavigator(drawerNavigatorRouteConfig, { edgeWidth: 100 });
+
+const switchNavigator = createSwitchNavigator({
+    Initial: InitialScreen,
+    AuthorizedApp: mainAppNavigator as any,
+    Auth: AuthScreen,
+    CreateAccount: CreateAccountScreen,
+    ForgotPassword: ForgotPasswordScreen
+});
+
+const AppContainer = createAppContainer(switchNavigator);
+
+export { AppContainer };
