ac38a5a9237a3f595cd6465c2a0d893854510269
Roman <romanmail.by@gmail.com>
2019-11-15 03:17:38 +0300

    a bit of refactoring

--- src/actions/initialize.ts
+++ src/actions/initialize.ts
@@ -22,37 +22,25 @@ export function onReady(workouts: DailyWorkout[]) {
 }
 
 async function getActiveWorkouts(date: string): Promise<DailyWorkout[]> {
+
+
     let prevDay: Date | string = new Date(date);
     prevDay.setDate(prevDay.getDate() - 1);
     prevDay = getShortDate(prevDay);
-    let prevWorkout = await AsyncStorage.getItem('workout' + prevDay).then(data => {
-        let workout: DailyWorkout = JSON.parse(data);
+
+    let nextDay: Date | string = new Date(date);
+    nextDay.setDate(nextDay.getDate() + 1);
+    nextDay = getShortDate(nextDay);
+
+    let prefix = 'workout';
+    let keyValuePairs = await AsyncStorage.multiGet([prefix + prevDay, prefix + date, prefix + nextDay]);
+    let workouts = keyValuePairs.map((pair) => {
+        let workout: DailyWorkout = JSON.parse(pair[1]);
         if (!workout) {
             workout = new DailyWorkout(date);
         }
         return workout;
     })
 
-    let nextDay: Date | string = new Date(date);
-    nextDay.setDate(nextDay.getDate() + 1);
-    nextDay = getShortDate(nextDay);
-
-    let nextWorkout = await AsyncStorage.getItem('workout' + nextDay).then(data => {
-        let workout: DailyWorkout = JSON.parse(data);
-        if (!workout) {
-            workout = new DailyWorkout(date);
-        }
-        return workout;
-    });
-
-    let activeWorkout = await AsyncStorage.getItem('workout' + date).then(data => {
-        let workout: DailyWorkout = JSON.parse(data);
-        if (!workout) {
-            workout = new DailyWorkout(date);
-        }
-        return workout;
-    });
-
-    return [prevWorkout, activeWorkout, nextWorkout];
-
+    return workouts;
 }
